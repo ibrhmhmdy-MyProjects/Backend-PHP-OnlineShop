48d704ff3ff349640c492a205da0c56c7ae4f518
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-09-10 13:53:29 +0300

    add Footer and Finish Project

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,12 @@
-
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-body-tertiary text-muted">
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © <?= date("Y") ?> Copyright:
+    Developer by <a class="text-reset fw-bold" href="https://ibrahimhamdy.com/">Ibrahim Hamdy</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
   </body>
 </html>
